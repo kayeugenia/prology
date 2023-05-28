0cf8b4f9250af85d352e26008131df3d62f19cab
kayeugenia <122983473+kayeugenia@users.noreply.github.com>
2023-05-29 00:42:22 +0800

    add delete button

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,139 +1,141 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import CreateProject from '../modals/CreateProject';
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import _ from "lodash";
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import _ from 'lodash';
 import './ProjectList.css';
-import { v4 } from "uuid";
+import { v4 } from 'uuid';
 
 const ProjectList = () => {
-    const [projectList, setProjectList] = useState({
-        todo: {
-            title: "Todo",
-            items: [],
-        },
-        "in-progress": {
-            title: "In Progress",
-            items: [],
-        },
-        done: {
-            title: "Completed",
-            items: [],
-        },
+  const [projectList, setProjectList] = useState({
+    todo: {
+      title: 'Todo',
+      items: [],
+    },
+    'in-progress': {
+      title: 'In Progress',
+      items: [],
+    },
+    done: {
+      title: 'Completed',
+      items: [],
+    },
+  });
+
+  const handleDragEnd = ({ destination, source }) => {
+    if (!destination) {
+      return;
+    }
+
+    if (destination.index === source.index && destination.droppableId === source.droppableId) {
+      return;
+    }
+    const itemCopy = { ...projectList[source.droppableId].items[source.index] };
+
+    setProjectList((prev) => {
+      prev = { ...prev };
+
+      prev[source.droppableId].items.splice(source.index, 1);
+
+      prev[destination.droppableId].items.splice(destination.index, 0, itemCopy);
+
+      return prev;
     });
+  };
 
-    const handleDragEnd = ({ destination, source }) => {
-        if (!destination) {
-          return;
-        }
+  const saveProject = (object) => {
+    setProjectList((prev) => {
+      return {
+        ...prev,
+        todo: {
+          title: 'Todo',
+          items: [
+            {
+              id: v4(),
+              name: object.Name,
+              desc: object.Description,
+              startDate: object.Date,
+              endDate: object.EndDate,
+            },
+            ...prev.todo.items,
+          ],
+        },
+      };
+    });
+  };
 
-        if (
-            destination.index === source.index &&
-            destination.droppableId === source.droppableId
-        ) {
-            return;
-        }
-        const itemCopy = { ...projectList[source.droppableId].items[source.index] };
+  const deleteProject = (columnId, itemId) => {
+    setProjectList((prev) => {
+        const updatedList = { ...prev };
+        const columnItems = updatedList[columnId].items;
+        const updatedItems = columnItems.filter((item) => item.id !== itemId);
+        updatedList[columnId].items = updatedItems;
+        return updatedList;
+    });
+  };
 
-        setProjectList((prev) => {
-            prev = { ...prev };
+  return (
+    <div className="container">
+      <div className="project-list">
+        <h3>Project List</h3>
+        <CreateProject save={saveProject} />
+      </div>
 
-            prev[source.droppableId].items.splice(source.index, 1);
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <div className="column-container">
+          {_.map(projectList, (data, key) => {
+            return (
+              <div key={key} className="column">
+                <h3>{data.title}</h3>
 
-            prev[destination.droppableId].items.splice(
-                destination.index,
-                0,
-                itemCopy
-            );
-
-            return prev;
-        });
-    };
-
-    const saveProject = (object) => {
-        setProjectList((prev) => {
-            return {
-                ...prev,
-                todo: {
-                    title: "Todo",
-                    items: [
-                        {
-                            id: v4(),
-                            name: object.Name,
-                            desc: object.Description,
-                            startDate: object.Date,
-                            endDate: object.EndDate,
-                        },
-                        ...prev.todo.items,
-                    ],
-                },
-            };
-        });
-    };
-    
-    return (
-        <div className="App">
-          <div>
-            <h3>Project List</h3>
-            <CreateProject save={saveProject} />
-          </div>
-      
-          <DragDropContext onDragEnd={handleDragEnd}>
-            {_.map(projectList, (data, key) => {
-              return (
-                <div key={key} className={"column"}>
-                  <h3>{data.title}</h3>
-      
-                  <Droppable droppableId={key}>
-                    {(provided, snapshot) => {
-                      return (
-                        <div
-                          ref={provided.innerRef}
-                          {...provided.droppableProps}
-                          className={"droppable-col"}
-                        >
-                          {data.items.map((el, index) => {
-                            return (
-                              <Draggable
-                                key={el.id}
-                                index={index}
-                                draggableId={el.id}
-                              >
-                                {(provided, snapshot) => {
-                                  console.log(snapshot);
-                                  return (
-                                    <div
-                                      className={`item ${
-                                        snapshot.isDragging && "dragging"
-                                      }`}
-                                      ref={provided.innerRef}
-                                      {...provided.draggableProps}
-                                      {...provided.dragHandleProps}
+                <Droppable droppableId={key}>
+                  {(provided, snapshot) => {
+                    return (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.droppableProps}
+                        className="droppable-col"
+                      >
+                        {data.items.map((el, index) => {
+                          return (
+                            <Draggable key={el.id} index={index} draggableId={el.id}>
+                              {(provided, snapshot) => {
+                                console.log(snapshot);
+                                return (
+                                  <div
+                                    className={`item ${snapshot.isDragging && 'dragging'}`}
+                                    ref={provided.innerRef}
+                                    {...provided.draggableProps}
+                                    {...provided.dragHandleProps}
+                                  >
+                                    {el.name}
+                                    <p className="desc">{el.desc}</p>
+                                    <p className="date">
+                                      Start-Date: {el.startDate} <br /> End-Date: {el.endDate}
+                                    </p>
+                                    <button
+                                        className="delete-btn"
+                                        onClick={() => deleteProject(key, el.id)}
                                     >
-                                      {el.name}
-                                      <p className="desc">{el.desc}</p>
-                                      <p className="date">
-                                        Start-Date: {el.startDate} <br /> End-Date:
-                                        {el.endDate}
-                                      </p>
-                                    </div>
-                                  );
-                                }}
-                              </Draggable>
-                            );
-                          })}
-                          {provided.placeholder}
-                        </div>
-                      );
-                    }}
-                  </Droppable>
-                </div>
-              );
-            })}
-          </DragDropContext>
+                                        Delete
+                                    </button>
+                                  </div>
+                                );
+                              }}
+                            </Draggable>
+                          );
+                        })}
+                        {provided.placeholder}
+                      </div>
+                    );
+                  }}
+                </Droppable>
+              </div>
+            );
+          })}
         </div>
-      );
-      
-      
+      </DragDropContext>
+    </div>
+  );
 };
 
 export default ProjectList;
--- src/modals/CreateProject.js
+++ src/modals/CreateProject.js
@@ -1,96 +1,125 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function CreateProject({save}) {
-    //Handling Modal
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    
-    const handleSave = (e) => {
-        e.preventDefault()
-        const object = {
-            Name: projectName,
-            Description: description,
-            Date: date, 
-            EndDate: endDate 
-        };
-        save(object);
-        handleClose();
-        setProjectName("");
-        setTaskDesc("");
-        setDate("");
-        setEndDate("");
+function CreateProject({ save }) {
+  // Handling Modal
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  // Handling Form Input
+  const [projectName, setProjectName] = useState("");
+  const [description, setTaskDesc] = useState("");
+  const [date, setDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [error, setError] = useState(false);
+
+  const inputRef = useRef(null);
+
+  const handleSave = (e) => {
+    e.preventDefault();
+
+    if (projectName.trim() === "") {
+      setError(true);
+      return;
     }
-    //Handling Form Input
-    const [projectName, setProjectName] = useState("");
-    const [description, setTaskDesc] = useState("");
-    const [date, setDate] = useState("");
-    const [endDate, setEndDate] = useState("");
+
+    const projectObject = {
+      Name: projectName,
+      Description: description,
+      Date: date,
+      EndDate: endDate
+    };
+    save(projectObject);
+    handleClose();
+    setProjectName("");
+    setTaskDesc("");
+    setDate("");
+    setEndDate("");
+    setError(false);
+  };
+
+  useEffect(() => {
+    if (show) {
+      inputRef.current.focus();
+    }
+  }, [show]);
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleSave(e);
+    }
+  };
 
   return (
     <>
-        <Button variant="primary" onClick={handleShow}>
-            Create Project
-        </Button>
+      <Button variant="primary" onClick={handleShow}>
+        Create Project
+      </Button>
 
-        <Modal show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-            <Modal.Title>Create Task</Modal.Title>
+          <Modal.Title>Create Task</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            <form>
-                <div className="form-group">
-                    <label>Project Name</label>
-                    <input 
-                        type="text" 
-                        className="form-control" 
-                        placeholder="Enter Project Name" 
-                        value = {projectName} 
-                        onChange = {(e) => setProjectName(e.target.value)}
-                        name = "taskName"/>
-                </div>
-                <div className="form-group">
-                    <label>Description</label>
-                    <textarea 
-                        rows = "5" 
-                        className="form-control" 
-                        placeholder="Enter Project Description" 
-                        value = {description} 
-                        onChange = {(e) => setTaskDesc(e.target.value)}
-                        name = "taskDesc"/>
-                </div>
-                <div className="form-group">
-                    <label>Start Date</label>
-                    <input 
-                        type="date"
-                        value={date}
-                        onChange = {(e) => setDate(e.target.value)}
-                    />
-                </div>
-                <div className="form-group">
-                    <label>End Date</label>
-                    <input 
-                        type="date"
-                        value={endDate}
-                        onChange = {(e) => setEndDate(e.target.value)}
-                    />
-                </div>
-
-            </form>
+          <form>
+            <div className="form-group">
+              <label>Project Name</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter Project Name"
+                value={projectName}
+                onChange={(e) => setProjectName(e.target.value)}
+                name="taskName"
+                onKeyDown={handleKeyDown}
+                ref={inputRef}
+              />
+              {error && (
+                <p className="text-danger">Please enter a project name.</p>
+              )}
+            </div>
+            <div className="form-group">
+              <label>Description</label>
+              <textarea
+                rows="5"
+                className="form-control"
+                placeholder="Enter Project Description"
+                value={description}
+                onChange={(e) => setTaskDesc(e.target.value)}
+                name="taskDesc"
+              />
+            </div>
+            <div className="form-group">
+              <label>Start Date</label>
+              <input
+                type="date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label>End Date</label>
+              <input
+                type="date"
+                value={endDate}
+                onChange={(e) => setEndDate(e.target.value)}
+              />
+            </div>
+          </form>
         </Modal.Body>
         <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-                Close
-            </Button>
-            <Button variant="primary" onClick={handleSave}>
-                Save Changes
-            </Button>
-            </Modal.Footer>
-         </Modal>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleSave}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
 
-export default CreateProject
+export default CreateProject;
